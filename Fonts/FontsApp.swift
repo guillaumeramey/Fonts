bf79f870e41177eef84328d457bfcf7d9d762e32
Guillaume Ramey <guillaume.ramey@icloud.com>
2023-10-15 09:30:13 +0200

    Initial Commit

---
+++ Fonts/FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontsApp.swift
+//  Fonts
+//
+//  Created by Guillaume Ramey on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
